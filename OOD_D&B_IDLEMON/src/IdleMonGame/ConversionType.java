ed612adcf8551d6b8b3307dda900918f9eee5070
= <evilsnowflake>
2016-12-08 10:47:09 -0500

    Filled out basic Idlemon and a few things in world.

--- OOD_D&B_IDLEMON/src/IdleMonGame/ConversionType.java
+++ OOD_D&B_IDLEMON/src/IdleMonGame/ConversionType.java
@@ -1,6 +1,9 @@
 package IdleMonGame;
 
 public interface ConversionType {
+	//This changes the color and conversions
+	//of the idlemon
 	String getColor();
-	void convertResources(int conversionRate);
+	int getType();
+	void convertResources(Double conversionRate, Double conversionRatio, World w);
 }
--- OOD_D&B_IDLEMON/src/IdleMonGame/IdleMon.java
+++ OOD_D&B_IDLEMON/src/IdleMonGame/IdleMon.java
@@ -1,21 +1,37 @@
 package IdleMonGame;
 
 public class IdleMon {
-	
+	//What resources are converted
 	ConversionType CT;
+	//cost of cathcing this idlemon
 	int catchCost;
+	//cost of keeping this idlemon
 	Double upkeepCost;
+	//How many resources are converted each time
 	Double conversionRate;
+	//how many more resources are made compared to used
 	Double conversionRatio;
+	//how strong is the idlemon(used in calculating conversionRate)
 	int level;
+	//Cost ratio of upgrading the idlemon to the next level
 	Double upgradeCostRatio;
-	int upgradeBenefitRatio;
+	//conversion rate gained per upgrade
+	int upgradeBenefit;
 	
 	int getUpgradeCost(){
 		
 		Double costMultiplier = new Double(Math.pow(upgradeCostRatio, level));
 		return (int)(catchCost * costMultiplier);
 	}
-	
+	void upgrade(World w){
+		int resource = w.resources.get(CT.getType());
+		int upgradeCost = getUpgradeCost();
+		if(resource >= upgradeCost){
+			w.resources.set(CT.getType(), resource - upgradeCost);
+			conversionRate = conversionRate + upgradeBenefit;
+		}else{
+			System.out.println("Not enough resources to upgrade");
+		}
+	}
 	
 }
